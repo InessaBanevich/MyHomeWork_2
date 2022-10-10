bf235893c068ae925d54332a2e659b8bd7b823fc
Inessa Banevich <InessaBanevich@yandex.by>
2022-10-10 13:50:13 +0300

    Task 10  added

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
+выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+void AnswerToTask(){
+
+int newNumber = (new Random()).Next(100,1000);
+Console.WriteLine($"Base number: {newNumber}");
+string numberAsString = newNumber.ToString();
+int result = int.Parse(numberAsString[1].ToString());
+Console.WriteLine($"Result number: {result}");
+
+}
+
+AnswerToTask();
